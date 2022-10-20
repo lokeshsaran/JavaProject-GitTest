f3f6e90045d2e687c64e4142399195da816aa79a
dungean4master <dungeanmaster@protonmail.com>
2022-10-20 14:48:31 +0530

    6th commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into USER_DETAIL (user_id,user_name,city,password) values (1,'tom','delhi','12345');
-insert into USER_DETAIL (user_id,user_name,city,password) values (2,'sunidhi','ranchi','12345');
+insert into USER_DETAIL (user_id,user_name,city,password) values (2,'sunidhi','ranchi','12343');
 insert into USER_DETAIL (user_id,user_name,city,password) values (3,'kunal','punjab','12345');
 insert into USER_DETAIL (user_id,user_name,city,password) values (4,'lokesh','harda','12345');
 insert into USER_DETAIL (user_id,user_name,city,password) values (5,'admin','bhopal','12345');
